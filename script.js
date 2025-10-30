a67716339b385e25c23fb3809d5b1fad030b93cd
Wasia1234 <kamilkama11221234@gmail.com>
2025-10-30 23:28:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const products = [
   
   { id: 1, name: "Białko WPC 900 g", price: 129.99, image: "https://sklep.kfd.pl/9607-large_default/kfd-premium-xxl-09-kg-wpc-80-900-g.jpg" },
-  { id: 2, name: "Kreatyna 500 g", price: 89.99, image: "https://sklep.kfd.pl/kfd-premium-creatine-500-g-kreatyna-monohydrat-p-4139.html?srsltid=AfmBOorV98GNAiFv71NbIiDBnRHUHdQ==" },
+  { id: 2, name: "Kreatyna 500 g", price: 89.99, image: "https://eko-familia.pl/userdata/public/gfx/30122/KFD‑Premium‑Creatine‑Kreatyna%2C‑smak‑jablko‑gruszka%2C‑500‑g‑eko‑familia.jpg"
+ },
   { id: 3, name: "Omega 3 90 kaps.", price: 59.99, image: "https://sklep.kfd.pl/18220-large_default/kfd-omega-3-90-kaps-najmocniejsza.jpg" },
   { id: 4, name: "Witaminy dla sportowców", price: 49.99, image: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcRlYzB-Q_Bo4zKz3yHZN2ll30S-e‑Zfk4eJfbfQr9cGow00RN‑F s_euHqfPWScNlHEMbzEoKPbcTBIUFUoWGI5SI12VYU‑PUk_4V1w4zyqQTxgjQCbC3QJrSMQdELDvR5NDfB3wog&usqp=CAc" },
   { id: 5, name: "BCAA 300 g", price: 79.99, image: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTCXCq79KXyavu6tcGV4b0Q9LlABNL5pSh‑PCIvTtOoGbyf1yZ9s‑9b6J_iTsydZNFSIB8kiJWbu6Ses_HZmrgAzAY5BFFLRcdEPb_dC4lKwSdrgKoqVS5_6w5ZQwFpTl_wcOsjQQ&usqp=CAc" },
